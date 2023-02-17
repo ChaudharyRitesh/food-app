3c987cd3d66d1b8d3b07f470ff91333e882bb362
albertofuent <albertofuentes282@gmail.com>
2023-02-17 15:34:59 -0500

    initial push

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-center">
+      <h1>Welcome To Food Delivery App</h1>
+    </div>
+  );
+}
+
+export default App;
