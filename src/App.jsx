d0ab251956f4b4ea9a9c9af3b21ec9f6fac13a20
ChaudharyRitesh <riteshchaudhary587@gmail.com>
2023-02-17 16:25:38 -0500

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import React from "react";
 function App() {
   return (
     <div>
-      <h1 className="text-center">Welcome To Food Delivery App</h1>
+      <h1 className="text-center text-gray-900">
+        Welcome To Food Delivery App
+      </h1>
     </div>
   );
 }
