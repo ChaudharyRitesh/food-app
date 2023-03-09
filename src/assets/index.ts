a7c588082b00723ccc2d68b2c2ba75889e07d6ca
ChaudharyRitesh <riteshchaudhary587@gmail.com>
2023-03-09 22:39:44 +0530

    hero section design

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,8 @@
 import logo from "./logo.png";
 import heroimage from "./heroImage.png";
+import cherry from "./cherry.png";
+import cherrySvg from "./cherry.svg";
+import playIcon from "./playIcon.png";
+import playIconSvg from "./playIcon.svg";
 
-export { logo, heroimage };
+export { logo, heroimage, cherry, cherrySvg, playIcon, playIconSvg };
--- src/layouts/Home/Header.tsx
+++ src/layouts/Home/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../../components/navbar/Navbar";
-import { heroimage } from "../../assets/index";
+import { heroimage, cherrySvg, playIconSvg } from "../../assets/index";
+import * as Unicons from "@iconscout/react-unicons";
 
 const Header = () => {
   return (
@@ -9,9 +10,51 @@ const Header = () => {
       <div className="hero">
         <div className="grid grid-cols-2 _lg:grid-cols-6">
           <div className="content-left">
-            <h1>
-              I'm a <span className="text-primary">Full Stack Developer</span>
-            </h1>
+            <div className="btnContainer pt-20">
+              <button className="outline-none border-none bg-[#FEE9DE] flex flex-row items-center justify-center gap-4 px-7 py-4 rounded-full">
+                <span className="text-[#EB5757] text-base leading-6 font-poppins font-medium capitalize _md:text-lg">
+                  More than faster
+                </span>
+                <img src={cherrySvg} alt="cherry" className="w-auto" />
+              </button>
+            </div>
+
+            <div className="info-text pt-10 flex flex-col flex-wrap">
+              <h1 className="text-[72px] text-[##333333] text-start font-poppins font-bold leading-[90px] w-full ">
+                Be The Fastest <br /> In Delivering <br /> Your{" "}
+                <span className="text-[#EB5757]">Food</span>
+              </h1>
+              <p className="text-lg text-[##333333] font-[500] text-justify font-poppins leading-7 pt-10 ">
+                Our job is to filling your tummy with delicious food <br /> and
+                with fast and free delivery
+              </p>
+            </div>
+
+            <div className="btn-group pt-10 flex flex-row gap-20 items-center ">
+              <button className="inline-block px-8 py-5 bg-[#EB5757] rounded-full text-lg text-white font-poppins font-medium">
+                Get Started
+              </button>
+              <div className="play flex flex-row flex-wrap gap-3 items-center justify-center">
+                <div className="playIcon w-16 h-16 shadow-md bg-[##d1d0d0] rounded-full flex items-center justify-center">
+                  {/* <Unicons.UilPlay size="23" color="#F2C94C" /> */}
+                  {/* <img src={playIconSvg} alt="play" className="w-auto" /> */}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    width="24"
+                    height="24">
+                    <path fill="none" d="M0 0h24v24H0z" />
+                    <path
+                      d="M19.376 12.416L8.777 19.482A.5.5 0 0 1 8 19.066V4.934a.5.5 0 0 1 .777-.416l10.599 7.066a.5.5 0 0 1 0 .832z"
+                      fill="rgba(242,201,76,1)"
+                    />
+                  </svg>
+                </div>
+                <span className="text-lg text-black font-poppins font-medium leading-7">
+                  Watch Video
+                </span>
+              </div>
+            </div>
           </div>
           <div className="content-right">
             <div className="hero-image">
