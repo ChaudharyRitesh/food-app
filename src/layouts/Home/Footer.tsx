2c0c124bdb7e96d51dfc02ad5d06c4c2c1bcacd3
ChaudharyRitesh <riteshchaudhary587@gmail.com>
2023-03-10 22:13:19 +0530

    header image fix

--- src/layouts/Home/Footer.tsx
+++ src/layouts/Home/Footer.tsx
@@ -285,7 +285,7 @@ const Footer = () => {
                 </a>
               </p>
 
-              <p className="mt-4 text-sm text-gray-500 sm:order-first sm:mt-0">
+              <p className="mt-4 text-md text-gray-500 sm:order-first sm:mt-0">
                 &copy; 2023 Fudo
               </p>
             </div>
