45c6f311a40bf1ec2cc75ae72b68817f51b1d7d3
ChaudharyRitesh <riteshchaudhary587@gmail.com>
2023-03-14 21:02:17 +0530

    image slider

--- src/layouts/Home/Menu.tsx
+++ src/layouts/Home/Menu.tsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 const Menu = () => {
-  return <div>The Menu Slider Will Go Here.</div>;
+  return (
+    <div>
+      <h2>Menu Slider Will Go Here </h2>
+      <div className="men-wrapper">
+        <div className="menu-item">
+          <div className="menu-item-image">
+            <img src="https://picsum.photos/200/300" alt="menu item" />
+          </div>
+          <div className="menu-item-image">
+            <img src="https://picsum.photos/200/300" alt="menu item" />
+          </div>
+          <div className="menu-item-image">
+            <img src="https://picsum.photos/200/300" alt="menu item" />
+          </div>
+          <div className="menu-item-image">
+            <img src="https://picsum.photos/200/300" alt="menu item" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Menu;
