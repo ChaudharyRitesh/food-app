9222a6986589975820c3a2e6d9ae0b81fc85bae7
ChaudharyRitesh <riteshchaudhary587@gmail.com>
2023-03-24 16:38:25 +0530

    add assets

--- src/layouts/Home/Services.tsx
+++ src/layouts/Home/Services.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Services = () => {
   return (
-    <div>
+    <>
       <h1 className="text-center text-[#EB5757] text-lg font-medium tracking-widest font-poppins uppercase">
         what we serve
       </h1>
-    </div>
+    </>
   );
 };
 
