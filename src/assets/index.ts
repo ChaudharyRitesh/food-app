9e261732605f33952aef77531f917cccbdf6a9a0
ChaudharyRitesh <riteshchaudhary587@gmail.com>
2023-03-24 17:58:00 +0530

    services section

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -5,5 +5,23 @@ import cherrySvg from "./cherry.svg";
 import playIcon from "./playIcon.png";
 import playIconSvg from "./playIcon.svg";
 import play from "./play.svg";
+import quality from "./quality.png";
+import delivery from "./delivery.png";
+import order from "./easyOrder.png";
+import yellowRect from "./yellowRect.png";
+import redRect from "./redRect.png";
 
-export { logo, heroimage, cherry, cherrySvg, playIcon, playIconSvg, play };
+export {
+  logo,
+  heroimage,
+  cherry,
+  cherrySvg,
+  playIcon,
+  playIconSvg,
+  play,
+  order,
+  quality,
+  delivery,
+  yellowRect,
+  redRect,
+};
