9fa1ad4af35fd6a5b8dc0129af15649611432202
ChaudharyRitesh <riteshchaudhary587@gmail.com>
2023-03-24 16:31:56 +0530

    add services section

---
+++ src/layouts/Home/Services.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Services = () => {
+  return (
+    <div>
+      <h1 className="text-center text-[#EB5757] text-lg font-medium tracking-widest font-poppins uppercase">
+        what we serve
+      </h1>
+    </div>
+  );
+};
+
+export default Services;
--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -4,11 +4,13 @@ import Footer from "./Home/Footer";
 import Slider from "../components/slider/Slider";
 
 import foodData from "../data/testimonialData.json";
+import Services from "./Home/Services";
 
 const LandingPage = () => {
   return (
     <div>
       <Header />
+      <Services />
       <Slider data={foodData} />
       <Footer />
     </div>
