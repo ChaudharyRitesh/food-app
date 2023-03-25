9de382665327be2a1d9adfb1a73c2c5b180c5ea6
ChaudharyRitesh <riteshchaudhary587@gmail.com>
2023-03-25 21:53:51 +0530

    implement swiper

---
+++ src/components/testimonials/Testimonal.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Testimonal = () => {
+  return (
+    <section class="bg-white">
+      <div class="mx-auto max-w-[1340px] px-4 py-16 sm:px-6 sm:py-24 lg:mr-0 lg:pl-8 lg:pr-0">
+        <div class="max-w-7xl items-end justify-between sm:flex sm:pr-6 lg:pr-8">
+          <h2 class="max-w-xl text-4xl font-bold tracking-tight sm:text-5xl">
+            Read trusted reviews from our customers
+          </h2>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Testimonal;
